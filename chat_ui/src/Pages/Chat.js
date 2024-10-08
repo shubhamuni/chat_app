71a9ba201c54c596f24b6b83f7d5421409d40c14
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-10-08 20:40:41 +0530

    sidebar for chat

--- chat_ui/src/Pages/Chat.js
+++ chat_ui/src/Pages/Chat.js
@@ -1,7 +1,14 @@
+import Sidebar from "../Components/Sidebar";
+
 const Chat = () => {
   
   return (
-    <div>Chat</div>
+    <div className='flex items-center justify-center h-screen bg-gray-100'>
+      <div className='bg-cover w-2/4 h-[calc(100vh-60px)] rounded-lg flex items-center justify-center' style={{ backgroundImage: "url(./bg.jpg)" }}>
+
+        <Sidebar/>
+      </div>
+    </div>
   )
 }
 
