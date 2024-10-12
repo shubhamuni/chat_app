dd0c1688f73a3070118a0c6aa7f7ef522067fae6
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-10-12 09:56:17 +0530

    sorting user messages with Id

--- chat_ui/src/Components/Form.js
+++ chat_ui/src/Components/Form.js
@@ -1,17 +1,28 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 
 const Form = () => {
     const [message, setMessage] = useState('');
+    const userId = window.localStorage.getItem('userId')
+    const sendMessage = async  () => {
+        try {
+            const response = await axios.post('http://localhost:9000/chat/message/send/')
+        } catch (error) {
+           console.log(error);
+            
+        }
+    }
   return (
       <div className="p-4 fixed bottom-5 right-0 left-24 flex justify-center">
-            <div className="flex items-center w-full max-w-md">
+          <form onSubmit={sendMessage}>
+              <div className="flex items-center w-full max-w-md">
                 {/* Input Field */}
                 <input 
                     type="text" 
                     value={message} 
                     onChange={(e) => setMessage(e.target.value)} 
                     placeholder="Type your message..." 
-                    className="flex-grow p-3 border border-gray-300 rounded-l-lg focus:outline-none"
+                    className="flex-grow w-full p-3 border border-gray-300 rounded-l-lg focus:outline-none"
                 />
                 
                 {/* Send Button */}
@@ -19,8 +30,9 @@ const Form = () => {
                     Send
                 </button>
             </div>
+            </form>
         </div>
   )
 }
 
-export default Form
+export default Form;
--- chat_ui/src/Pages/Chat.js
+++ chat_ui/src/Pages/Chat.js
@@ -4,18 +4,19 @@ import Form from "../Components/Form";
 
 const Chat = ({socket}) => {
   const [chatInitiated, setChatInitiated] = useState(false);
-  const [chat, setChat] = useState([])
+  const [chat, setChat] = useState([]);
+  const [receiverId, setReceiverId] = useState();
   
   return (
     <div className='flex items-center justify-center h-screen bg-gray-100'>
       <div
         className='bg-cover w-2/4 h-[calc(100vh-60px)] rounded-lg flex' style={{ backgroundImage: "url(./bg.jpg)" }}>
 
-        <Sidebar setChatInitiated={setChatInitiated} setChat={setChat} socket={socket}/>
+        <Sidebar setChatInitiated={setChatInitiated} setChat={setChat} socket={socket} setReceiverId={setReceiverId}/>
         <div className="w-3/4 bg-white flex flex-col bg-opacity-20 realative">
           {chatInitiated ? <div>
             <p>Chat Initiated</p>
-            <Form />
+            <Form setReceiverId={setReceiverId} />
           </div> : <div className="flex justify-center items-center h-full">
             <h2 className='text-3xl py-3 bg-white bg-opacity-70 font-bold text-gray-700 rounded-lg'>-----Welcome-----</h2>
           </div> }
