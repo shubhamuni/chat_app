296606455c3a60f8e6bf7e15b4bfc0d2e4f82ef4
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-10-14 22:33:25 +0530

    Update socket.js

--- chatServer/sockets/socket.js
+++ chatServer/sockets/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://chat-app-17cu.vercel.app',
+        origin: '*',
         methods: ['GET', 'POST'],
         credentials: true
     }
